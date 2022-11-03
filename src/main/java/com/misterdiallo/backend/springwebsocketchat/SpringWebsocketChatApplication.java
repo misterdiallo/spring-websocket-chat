d08098441034fb36bc816c98f43af66c55a6937f
Mister Diallo <77695206+misterdiallo@users.noreply.github.com>
2022-11-04 04:07:58 +0800

    initial String project (dep: websocket, lombok,web)

---
+++ src/main/java/com/misterdiallo/backend/springwebsocketchat/SpringWebsocketChatApplication.java
@@ -0,0 +1,13 @@
+package com.misterdiallo.backend.springwebsocketchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketChatApplication.class, args);
+	}
+
+}
